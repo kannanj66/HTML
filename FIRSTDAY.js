9eb19ece131de0fd5bb2478e1e54aca9ceb5179f
kannanj66 <kannanmbaa2005@gmail.com>
2024-08-23 12:48:54 +0530

    Add files via upload

---
+++ FIRSTDAY.js
@@ -0,0 +1,25 @@
+let Empname='krishna';
+console.log(Empname);
+
+let age=40;
+console.log(age)
+
+let value='I AM A PROFESSOR';
+console.log(value)
+
+let profession=true
+console.log(profession)
+
+let salary=95000.50;
+console.log(salary)
+
+let phonenumber=['123','456','789'];
+console.log(phonenumber)
+
+
+const Empdetails ={
+    name='krishna',age=40,value='I AM A PROFESSOR',
+    profession=true,salary=95000.50,phonenumber=['123','456','789']
+
+}
+console.log(Empdetails.name)
